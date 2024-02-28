fd20d8aec0ec95c0f5ad38fd7efdce7ff8ca945f
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2024-02-28 15:13:14 -0800

    Add JS animation

--- WebAppTutorial/wwwroot/js/site.js
+++ WebAppTutorial/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var id = null;
+function move() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + 'px';
+        }
+    }
+} 
